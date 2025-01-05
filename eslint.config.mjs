9c5bd8d9d4cb745b234db6c4bae27258143c6f5c
LubovKrasheninnikova <altukhovalv@yandex.ru>
2025-01-05 13:43:46 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import eslintconfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  js.configs.recommended,
+  eslintconfigPrettier,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    }
+  }
+];
