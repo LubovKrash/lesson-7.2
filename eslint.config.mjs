de0af6eb32eaba23a243829d9d176272db5af81f
LubovKrasheninnikova <altukhovalv@yandex.ru>
2025-01-05 14:32:31 +0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import js from "@eslint/js";
-import eslintconfigPrettier from "eslint-config-prettier";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import js from '@eslint/js';
+import eslintconfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   js.configs.recommended,
@@ -17,8 +16,8 @@ export default [
 
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    }
-  }
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
 ];
