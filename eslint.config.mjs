80ca494c084789a6cf548237ea9cbd440ac158a1
LubovKrasheninnikova <altukhovalv@yandex.ru>
2025-01-09 23:49:54 +0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,28 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginReact from "eslint-plugin-react";
-import configPrettier from "eslint-config-prettier";
+import configPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        rules: {
-            'no-var': 'error',
-            semi: 'error',
-            indent: 'error',
-            'no-multi-spaces': 'error',
-            'space-in-parens': 'error',
-            'no-multiple-empty-lines': 'error',
-            'prefer-const': 'error',
-            'no-use-before-define': 'error',
-        },
+  {
+    languageOptions: {
+      globals: globals.browser,
     },
-    configPrettier,
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'no-var': 'error',
+      semi: ['error', 'always'],
+      indent: 'error',
+      'no-multi-spaces': 'error',
+      'space-in-parens': 'error',
+      'no-multiple-empty-lines': 'error',
+      'prefer-const': 'error',
+      'no-use-before-define': 'error',
+      'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    },
+  },
+  pluginJs.configs.recommended,
+  configPrettier,
 ];
